7e9fa8e6126bf47ad666108f74fbf9aed7fe5799
xiepengxiang <xiepengxiang@365ime.com>
2016-07-13 00:25:01 +0800

    添加triangleLayer

--- MXLayerAnimation/MXLayerAnimation/MXLayer/MXAnimationView.m
+++ MXLayerAnimation/MXLayerAnimation/MXLayer/MXAnimationView.m
@@ -8,10 +8,12 @@
 
 #import "MXAnimationView.h"
 #import "CircleLayer.h"
+#import "TriangleLayer.h"
 
 @interface MXAnimationView()
 
 @property (nonatomic, strong) CircleLayer *circleLayer;
+@property (nonatomic, strong) TriangleLayer *triangleLayer;
 
 @end
 
@@ -29,8 +31,11 @@
 
 - (void)addCircleLayer
 {
-    [self.layer addSublayer:self.circleLayer];
-    [_circleLayer contractAnimation];
+//    [self.layer addSublayer:self.circleLayer];
+//    [_circleLayer contractAnimation];
+    
+    [self.layer addSublayer:self.triangleLayer];
+    [_triangleLayer triangleAnimate];
 }
 
 #pragma mark - lazy load
@@ -41,6 +46,15 @@
     }
     
     return _circleLayer;
+}
+
+-(TriangleLayer *)triangleLayer
+{
+    if (!_triangleLayer) {
+        _triangleLayer = [[TriangleLayer alloc] init];
+    }
+    
+    return _triangleLayer;
 }
 
 @end
---
+++ MXLayerAnimation/MXLayerAnimation/MXLayer/layer/TriangleLayer.h
@@ -0,0 +1,15 @@
+//
+//  TriangleLayer.h
+//  MXLayerAnimation
+//
+//  Created by 谢鹏翔 on 16/7/8.
+//  Copyright © 2016年 ime. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface TriangleLayer : CAShapeLayer
+
+- (void)triangleAnimate;
+
+@end
